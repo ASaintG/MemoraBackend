703874307f0d6c44caf5968fefff4cf221393e3b
ASaintG <78000780+ASaintG@users.noreply.github.com>
2024-02-23 15:35:46 -0600

    agregar ruta del login

--- src/app.js
+++ src/app.js
@@ -1,26 +1,41 @@
-import express, { query } from 'express';
-import {pool}from './db.js'
-import {PORT} from './config.js'
-const app =express();
+import express from 'express';
+import { pool } from './db.js';
+import { PORT } from './config.js';
 
+const app = express();
 
-app.get('/', async(req, res) => {
-    const [rows]= await pool.query('Select * FROM Usuarios')
+app.use(express.json()); 
+
+app.get('/', async (req, res) => {
+    const [rows] = await pool.query('SELECT * FROM Usuarios');
     res.json(rows);
 });
 
+app.post('/login', async (req, res) => {
+    const { Email, Password } = req.body;
+
+    // Verificar si el usuario existe en la base de datos
+    const [rows] = await pool.query('SELECT * FROM Usuarios WHERE Email = ? AND Password = ?', [Email, Password]);
+
+    if (rows.length === 1) {
+        // Usuario autenticado
+        res.json({ message: 'Login exitoso', usuario: rows[0] });
+    } else {
+        // Credenciales inválidas
+        res.status(401).json({ error: 'Credenciales inválidas' });
+    }
+});
+
 app.get('/ping', async (req, res) => {
-    const [Result] =await pool.query('SELECT "hello world " as RESULT')
-    console.log (Result);
+    const [Result] = await pool.query('SELECT "hello world " as RESULT');
+    console.log(Result);
     res.json(Result[0]);
-})
+});
 
-app.post('/createuser', async (req, res)=>{
-    const Result = await pool.query('INSERT INTO Usuarios(NombreUsuario,Password,NombreCompleto,Email) VALUES ("Saint","123456","Anthony Saint Hernandez","anthony@gmail.com")')
+app.post('/createuser', async (req, res) => {
+    const Result = await pool.query('INSERT INTO Usuarios(NombreUsuario,Password,NombreCompleto,Email) VALUES ("Saint","123456","Anthony Saint Hernandez","anthony@gmail.com")');
     res.json(Result);
-})
+});
 
-app.listen(PORT)
-console.log('server on port 3000 ',PORT);
-
-//logging, morgan, winston
+app.listen(PORT);
+console.log('server on port 3000 ', PORT);
--- src/config.js
+++ src/config.js
@@ -9,3 +9,5 @@ export const DB_PASSWORD= process.env.DB_PASSWORD || 'pgm11-g2-+24'
 export const DB_NAME= process.env.DB_NAME ||'pst079_PGMII'
 
 export const DB_PORT= process.env.DB_PORT || 3306
+
+
